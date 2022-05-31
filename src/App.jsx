d69131f802cf5264be6cd1d02a74d88acb3f194e
NiazMorshed2007 <niazmorshed2007@gmail.com>
2022-05-31 10:35:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="p-10 px-28">
+      <h1 className="text-3xl underline underline-offset-8 decoration-violet-500">
+        Exercises
+      </h1>
+    </main>
+  );
+};
+
+export default App;
